71042ed1909b548d7dc2445d1d9121c34d9c0eb8
Mert Kaya <mertkaya1033@gmail.com>
2023-12-01 16:13:32 -0500

    fix localStorage issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import SkillList from "./components/skill-list";
 
 import CircularProgress from "./components/circular-progress";
@@ -26,9 +26,12 @@ function twoDecimal(num: number) {
 
 export default function HomePage() {
   const router = useRouter();
-  if (!localStorage.getItem("isLoggedIn")) {
-    router.push("/login");
-  }
+  useEffect(() => {
+    if (!localStorage.getItem("isLoggedIn")) {
+      router.push("/login");
+    }
+  }, [router]);
+
   const searchParams = useSearchParams();
 
   const date = new Date();
